15350a21dac5e27be786261605eb84943ad9cee9
kscardinal <kschang@cardinalhs.net>
2025-09-18 12:23:25 -0400

    added more files and style sheets

--- speedTests.php
+++ speedTests.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Speed Test | Sonnaz Group</title>
+    <title>Speed Tests | Sonnaz Group</title>
     <link rel="icon" type="image/png" sizes="256x256" href="assets/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body style="background: #1B1C1E">
 <?php
 include 'includes/pingTest.html';
+include 'includes/downloadTest.html'
 ?>
 </body>
 </html>
